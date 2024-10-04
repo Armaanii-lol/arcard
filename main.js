72e619a2be8a1331da300068a3459dc00fea261c
Armaanii-lol <armaan.sh.study@gmail.com>
2024-10-04 12:22:53 +0530

    Resubmit

--- main.js
+++ main.js
@@ -1,39 +1,42 @@
 const THREE = window.MINDAR.IMAGE.THREE;
 
 document.addEventListener('DOMContentLoaded', () => {
-	const start = async () => {
-		const video = document.createElement("video");
-		video.setAttribute("src", "./myVideo.mp4");
-		video.setAttribute("loop", "")
+const start = async () => {
+	const video = document.createElement("video");
+	video.setAttribute("src", "./myVideo.mp4");
+	video.setAttribute("loop", "");
 
-		video.oncanplay = () => {
-			anchor.onTargetFound = () => {
-				video.play();
-			}
-				anchor.onTargetLost = () => {
-				video.pause();
-			}
+	video.oncanplay = () => {
+		anchor.onTargetFound = () => {
+			video.play();
 		}
-		const mindarThree = new window.MINDAR.IMAGE.MindARThree({
-			container: document.body,
-			imageTarget: './josephs.mind'
-		});
 
-	const {renderer, scene, camera} = mindarThree;
+		anchor.onTargetLost = () => {
+			video.pause();
+		}
+				
+	}
+	const mindarThree = new window.MINDAR.IMAGE.MindARThree({
+	container: document.body, 
+	imageTargetSrc: './josephs.mind'
+});
 
-	const geometry = new THREE.PlaneGeometry(1, 1);
-	const videoTexture = new THREE.VideoTexture(video);
-	const material = new THREE.MeshBasicMaterial({map: videoTexture, side: THREE.FrontSide, toneMapped: false});
-	const plane = new THREE.Mesh(geometry, material);
+const {renderer, scene, camera} = mindarThree;
 
-	const anchor = mindarThree.addAnchor(0);
-	anchor.group.add(plane);
+const geometry = new THREE.PlaneGeometry(1, 1);
+const videoTexture = new THREE.VideoTexture(video);
+const material = new THREE.MeshBasicMaterial({map: videoTexture, side: THREE.FrontSide, toneMapped: false});
+const plane = new THREE.Mesh(geometry, material);
 
-	await mindarThree.start();
+const anchor = mindarThree.addAnchor(0);
+anchor.group.add(plane);
 
-	renderer.setAnimationLoop( () => {
-		renderer.render(scene, camera);
-	});
+await mindarThree.start();
+
+
+renderer.setAnimationLoop( () => {
+	renderer.render(scene, camera);
+});
 
 }
 
